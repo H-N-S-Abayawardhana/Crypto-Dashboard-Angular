365f4c355aaea6309eafe42d19eb9ae4c60f9423
H-N-S-Abayawardhana <hnsabayawardhana@gmail.com>
2025-06-07 22:11:39 +0530

    adjust the spaces

--- src/app/components/dashboard/dashboard.component.spec.ts
+++ src/app/components/dashboard/dashboard.component.spec.ts
@@ -6,16 +6,20 @@ describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [DashboardComponent]
     })
     .compileComponents();
 
+
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
