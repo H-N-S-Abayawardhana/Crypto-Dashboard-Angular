989f61ebb4bec718a66ed3a0e8b93def8c2e4410
H-N-S-Abayawardhana <hnsabayawardhana@gmail.com>
2025-06-07 22:12:05 +0530

    updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -8,17 +10,22 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+
+
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
+
+
   it(`should have the 'crypto-dashboard' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('crypto-dashboard');
   });
+
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
